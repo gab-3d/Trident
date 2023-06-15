9fead5a795116e7e8088bc2de8f0b39d0bfc3af9
GAB 3D <gabrielecastellani@hotmail.com>
2023-06-15 21:46:03 +0200

    New config

--- shaper/adxl_shape_y.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o ~/printer_data/config/shaper/ultima_calibrazione_Y.png
--- shaper/clear_old_raw_data.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find /tmp -maxdepth 1 -name 'calibration_data*' -delete
